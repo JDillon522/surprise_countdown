cf452a8981e47a91d9734e185a1c9043d2c733d3
Tyler Saklad <tyler.saklad@gmail.com>
2017-02-05 19:10:37 +0200

    dillon messed me up. bloody hell

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,35 @@
 // Take a look at the documentation for the Date() built in object right hurr:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-var today = Date.now();
+var tenSec = moment().second(10);
+var now = moment();
 
-$('#one').html(today);
+$('#one').html(tenSec);
+
+var showshit = function(hide, show){
+  $(hide).hide()
+  $(show).show()
+}
+showshit('#two','#one')
+
+var dateconfig = function(gdate) {
+  if(today >= gdate) {
+
+  }else {
+
+  }
+}
+
+var figureDate = function(now, bench) {
+
+  if (now < bench) {
+    console.log('GREATER')
+  } else {
+    console.log('WAITING')
+  }
+}
+
+setInterval(function() {
+  // figureDate(now, tenSec)
+  console.log(tenSec)
+  console.log(now)
+}, 1000);
