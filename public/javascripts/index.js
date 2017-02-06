da0b889bcd9291490bc19896946661c33da26b66
Joseph Dillon <joseph.dillon.522@gmail.com>
2017-02-06 20:44:00 +0200

    setting tyler up for success

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,35 +1,21 @@
 // Take a look at the documentation for the Date() built in object right hurr:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-var tenSec = moment().second(10);
+var fiveSec = moment().add(5, 'seconds');
+var fifteenSec = moment().add(15, 'seconds');
 var now = moment();
 
-$('#one').html(tenSec);
 
-var showshit = function(hide, show){
-  $(hide).hide()
-  $(show).show()
-}
-showshit('#two','#one')
-
-var dateconfig = function(gdate) {
-  if(today >= gdate) {
-
-  }else {
-
-  }
-}
-
-var figureDate = function(now, bench) {
-
-  if (now < bench) {
-    console.log('GREATER')
+var figureDate = function(bench, selector) {
+  if (moment() >= bench) {
+    $(selector + ' .visible').hide();
+    $(selector + ' .hidden').show();
   } else {
-    console.log('WAITING')
+    console.log('WAITING on ' + selector);
   }
 }
 
+// setInterval(function, timeInMilliseconds) will execute the <function> every <timeInMilliseconds>
 setInterval(function() {
-  // figureDate(now, tenSec)
-  console.log(tenSec)
-  console.log(now)
+  figureDate(fiveSec, '#firstSurprise');
+  figureDate(fifteenSec, '#secondSurprise');
 }, 1000);
